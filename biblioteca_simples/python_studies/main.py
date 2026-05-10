3063218729b648b5e059b37343dd016e84691ebf
palermo <joao.palermo878@gmail.com>
2026-05-10 17:23:50 -0300

    (version 0.1.2)

--- biblioteca_simples/python_studies/main.py
+++ biblioteca_simples/python_studies/main.py
@@ -5,6 +5,7 @@
 
 #===============================================================================================================
 # DATA BASE
+
 users = [
     {"name_user" : "joao", "password" : "1234", "profile" : "reader"},
     {"name_user" : "Roco", "password" : "1234", "profile" : "librarian"},
@@ -13,19 +14,20 @@ users = [
 
 books = [
     {"name_book" : "Dune", "category" : "Fiction", "available" : True},
-    {"name_book" : "Star wars", "category" : "Fiction", "available" : False},
+    {"name_book" : "Star wars", "category" : "Fiction", "available" : True},
     {"name_book" : "Sons of Dune", "category" : "Fiction", "available" : False},
 ]
 
 loans = [
     {"user" : "joao", "book" : "Star wars", "returned" : True},
     {"user" : "Zuan", "book" : "Sons of Dune", "returned" : False},
-    {"user" : "joao", "book" : "Dune", "returned" : False}
+    {"user" : "joao", "book" : "Dune", "returned" : True}
 ]
 
 #===============================================================================================================
 # Cadastro de usuários
-def users_register():
+
+def Users_register():
     
     print("=== Regitre sua Conta ===")
     print("qual é o seu nome?")
@@ -58,7 +60,8 @@ def users_register():
     
 #===============================================================================================================
 # Login
-def do_login():
+
+def Do_login():
     print("=== LOGIN ===")
     name = input("Digite seu nome: ").strip()
     password1 = input("Digite sua senha ").strip()
@@ -74,7 +77,7 @@ def do_login():
 #===============================================================================================================
 # Registrar livro
 
-def book_register():
+def Book_register():
     
     print("=== Registro de Livros ===")
     print("=== Digite o nome do livro ===")
@@ -91,11 +94,41 @@ def book_register():
     New_Book = {"name_book" : Name_book, "category" : Book_category, "available" : True}
     books.append(New_Book)
     print(f"Livro '{Name_book}', cadastrado com sucesso!")
+
+#===============================================================================================================
+# Pesquisar Livro e pesquisar categoria
+
+def Search_book():
+    print("=== Pesquisa de livros ===")
+    print("Digite o nome do livro que vc quer pesquisar")
+    Search = input(" ")
     
+    for i in books:
+        if i["name_book"] == Search:
+            print(f"Livro Disponível: {i['name_book']} - Categoria: {i['category']} - Disponível: {'Sim' if i['available'] else 'Não'}")
+            return
+        
+def Search_category():
+    print("=== Pesquisar categoria ===")
+    print("Digite a categoria do livro que vc quer pesquisar")
+    Search = input(" ")
+    
+    for i in books:
+        if i["category"] == Search:
+            print(f"{i['name_book']} - Categoria: {i['category']} - Disponível: {'Sim' if i['available'] else 'Não'}")
 
-book_register()
+#===============================================================================================================
+# Registrar emprestimo
 
-print(books)
-
-
-
+#def Loan_register():
+#    print("=== Empretimos de livros===")
+#    print("Digite o nome do livro que vc quer pegar empretado")
+#    loan_book = input(" ")
+#    
+#    for i in books and i["available"] == True:
+#        if i["name_book"] == loan_book:
+#            print()(f"Livro '{i['name_book']}' emprestado com sucesso!")
+#        else:
+#            print("xxx Livro indisponível para empréstimo xxx")
+#            return
+#            
